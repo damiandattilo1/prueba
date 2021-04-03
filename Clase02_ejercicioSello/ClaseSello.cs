8a8ab5559398cecfac82ef34236738ba711102d2
damiandattilo1 <60588905+damiandattilo1@users.noreply.github.com>
2021-04-03 20:03:12 -0300

    Add files via upload

---
+++ Clase02_ejercicioSello/ClaseSello.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase02_ejercicioSello
+{
+    class ClaseSello
+    {
+        public static string Mensaje;
+        public static ConsoleColor color;
+
+        public static string Imprimir()
+        {
+            return ClaseSello.ArmarFormatoMensaje();
+        }
+        public static void Borrar()
+        {
+              ClaseSello.Mensaje ="";
+        }
+        public static void ImprimirEnColor()
+        {
+            Console.ForegroundColor = ClaseSello.color;
+            Console.WriteLine(ClaseSello.Imprimir());
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        private static string ArmarFormatoMensaje()
+        {
+            string AuxString;
+            int StrLength = ClaseSello.Mensaje.Length;
+            if(StrLength>0)
+            {
+                AuxString = new string('*', StrLength + 2) + "\n";
+                AuxString += "*" + ClaseSello.Mensaje + "*" + "\n";
+                AuxString += new string('*', StrLength + 2);
+                return AuxString;
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ Clase02_ejercicioSello/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase02_ejercicioSello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse("5");
+            ClaseSello.Mensaje = "hola mundo";
+            Console.WriteLine(ClaseSello.Imprimir());//retorna string
+            ClaseSello.Borrar();
+            Console.WriteLine(ClaseSello.Imprimir());//retorna string
+
+            ClaseSello.Mensaje = "hola mundo C#";
+            ClaseSello.color = ConsoleColor.Red;
+            ClaseSello.ImprimirEnColor();
+            Console.WriteLine(ClaseSello.Imprimir());//retorna string
+
+            ClaseSello.Mensaje = "hola mundo";
+            ClaseSello.color = ConsoleColor.Red;
+            ClaseSello.ImprimirEnColor();
+            Console.WriteLine(ClaseSello.Imprimir());
+            
+            Console.ReadKey();
+        }
+    }
+}
