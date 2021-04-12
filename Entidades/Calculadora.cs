bfa093171d8ccae66add5818725493634cde2d04
damiandattilo1 <60588905+damiandattilo1@users.noreply.github.com>
2021-04-11 21:53:07 -0300

    Add files via upload

---
+++ Entidades/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Calculadora
+    {/*
+        public static double Operar (double num1, double num2, string operador)
+        {
+            operador = Calculadora.ValidarOperador((char)operador);
+            if(operador == '+')
+            {
+                return num1 + num2;
+            }
+            else if(operador == '-')
+            {
+                return num1 - num2;
+            }
+            else if(operador == '/')
+            {
+                return num1 / num2;
+            }
+            else if (operador == '*')
+            {
+                return num1 * num2;
+            }
+
+        }
+        private static string ValidarOperador(char operador)
+        {
+            if (operador == '+' || operador == '-' || operador == '/' || operador == '*')
+            {
+                return operador;
+            }
+            else
+            {
+                return '+';
+            }
+        }
+        public static implicit operator char (string s)
+        {
+            return s;
+        }
+
+        public static explicit operator string (char c)
+        {
+            return c;
+        }
+       */
+    }
+}
---
+++ Entidades/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero n1 = new Numero(5);
+            Numero n2 = new Numero("2,54");
+
+            double numeroDouble = 40;
+            string numeroString = "77";
+
+            //string binarioString = "a";
+
+            Console.WriteLine("{0}", Numero.DecimalBinario(numeroDouble));
+            Console.WriteLine("{0}\n", Numero.BinarioDecimal(Numero.DecimalBinario(numeroDouble)));
+
+            Console.WriteLine("{0}", Numero.DecimalBinario(numeroString));
+            Console.WriteLine("{0}\n", Numero.BinarioDecimal(Numero.DecimalBinario(numeroString)));
+          
+            /*if(Numero.esBinario(binarioString))
+            {
+                Console.WriteLine("SI binario");
+            }
+            else
+            {
+                Console.WriteLine("NO binario");
+            }*/
+
+            Console.ReadKey();
+        }
+    }
+}
